3ef9073384f3a8c070ef41a1ca0f4804fedf97ce
Darina Kalina <kalinadarina95@gmail.com>
2019-09-07 21:16:35 +0300

    Simple visualization aded
    DB Model and simple visualization added

--- WeatherInfo/WeatherInfo/Controllers/HomeController.cs
+++ WeatherInfo/WeatherInfo/Controllers/HomeController.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WeatherInfo.Models;
 
 namespace WeatherInfo.Controllers
 {
     public class HomeController : Controller
     {
+        private WeatherDBEntities _db = new WeatherDBEntities();
         public ActionResult Index()
         {
             return View();
         }
+
         public ActionResult Visualize()
         {
-            return View();
+            return View(_db.WeatherInfo.ToList());
         }
 
         public ActionResult Upload()
--- WeatherInfo/WeatherInfo/Properties/AssemblyInfo.cs
+++ WeatherInfo/WeatherInfo/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("730cc6a8-ac60-4c9f-8cc2-6528c122168b")]
+[assembly: Guid("c24095ce-b7a9-4dc7-a9d4-14e5eab135d2")]
 
 // Version information for an assembly consists of the following four values:
 //
